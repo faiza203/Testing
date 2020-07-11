d06a14f808dbb14e75ac6c8124f6486430cf82c0
faiza203 <60883181+faiza203@users.noreply.github.com>
2020-07-11 09:41:39 +0500

    simple testing

---
+++ test.js
@@ -0,0 +1,24 @@
+const { forEach, map } = require('./script.js');
+
+let sum = 0;
+forEach([1, 2, 3], value => {
+  sum += value;
+});
+
+if (sum !== 6) {
+  throw new Error('Expected summing array to equal 6');
+}
+
+const result = map([1, 2, 3], value => {
+  return value * 2;
+});
+
+if (result[0] !== 2) {
+  throw new Error(`Expected to find 2, but found ${result[0]}`);
+}
+if (result[1] !== 4) {
+  throw new Error(`Expected to find 4, but found ${result[0]}`);
+}
+if (result[2] !== 6) {
+  throw new Error(`Expected to find 6, but found ${result[0]}`);
+}
